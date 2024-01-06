55e9a178d38ebea7fd6c08043c61bd5d7b7a8f72
Umanda Ravimal <umandaravimal@gmail.com>
2024-01-06 14:58:36 +0530

    Completed the huffmanTree

--- HuffmanTree.java
+++ HuffmanTree.java
@@ -1,3 +1,4 @@
+import java.util.HashMap;
 import java.util.Map;
 import java.util.PriorityQueue;
 
@@ -5,15 +6,57 @@ public class HuffmanTree {
     private PriorityQueue <HuffmanNode> minHeap;
 
     public HuffmanTree(){
+        //Create a priority Queue
         minHeap = new PriorityQueue<>((a,b) -> a.frequency - b.frequency);
     }
 
-    public void BuildHuffmanTree(String data){
-        Map <Character , Integer> freqMap = countFrequencies(data);
+    public HuffmanNode buildHuffmanTree(String data){
+        //count frequenceis of character in the data
+        Map <Character , Integer> frequencyMap = countFrequencies(data);
 
-        buildPriorityQueue(freqMap);
+        //Build Huffman Tree
+        buildPriorityQueue(frequencyMap);
 
-        return buildTree;
+        return buildTree();
+    }
+
+    private Map <Character, Integer> countFrequencies (String data){
+        Map <Character , Integer > frequencyMap = new HashMap<>();
+
+        for (char c : data.toCharArray()){
+            frequencyMap.put(c, frequencyMap.getOrDefault(c, 0)+1);
+        }
+
+        return frequencyMap;
+    }
+
+    private void buildPriorityQueue(Map<Character, Integer>frequencMap){
+
+        for(Map.Entry <Character, Integer> entry : frequencMap.entrySet()){
+
+            HuffmanNode Node = new HuffmanNode(entry.getKey(), entry.getValue());
+
+            minHeap.add(Node);
+        }
+    }
+
+    private HuffmanNode buildTree(){
+
+        while (minHeap.size() > 1) {
+
+            HuffmanNode left = minHeap.poll();
+            HuffmanNode right = minHeap.poll();
+
+            HuffmanNode internalNode = new HuffmanNode('\0', left.frequency + right.frequency);
+
+            internalNode.left = left;
+            internalNode.right = right;
+
+            minHeap.add(internalNode);
+        }
+
+        return minHeap.poll();
+
     }
 
 
